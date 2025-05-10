41b221c4989ad7803f86a325374d59bcdc96333b
t22cs043-0 <kkarinn114@gmail.com>
2025-05-10 17:53:33 +0900

    [add]

--- data_collection/merge_PSS.py
+++ data_collection/merge_PSS.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Person 1 のデータ読み込みとラベル追加
-df1 = pd.read_csv("person1_features.csv")
+df1 = pd.read_csv("person1_features.csv")#komorikarin
 original_score=39
 normalized_score = original_score / 56.0 * 10.0
 df1["stress_score"] = normalized_score  # Person 1 のPSSスコア（スケーリング済み）
 
 # Person 2 のデータ
-df2 = pd.read_csv("person2_features.csv")
+df2 = pd.read_csv("person2_features.csv")#kawamurashungo
 original_score=38
 normalized_score = original_score / 56.0 * 10.0
 df2["stress_score"] = normalized_score  # Person 2 のPSSスコア（スケーリング済み）
